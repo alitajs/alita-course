d41f8c4e673e244eb4495f586859955e28f0e4cc
xiaohuoni <448627663@qq.com>
2019-05-31 15:40:03 +0800

    Code refactoring

--- myApp/src/models/hero.ts
+++ myApp/src/models/hero.ts
@@ -1,9 +1,7 @@
-import { query } from '@/services/api';
-
+import { queryHeroList, getHeroDetails } from '@/services/api';
 import { Effect } from '@/models/connect';
 import { Reducer } from 'redux';
 import { Subscription } from 'dva';
-import { request } from 'alita';
 
 export interface HeroModelState {
   name: string;
@@ -14,7 +12,6 @@ export interface HeroModelType {
   namespace: 'hero';
   state: HeroModelState;
   effects: {
-    query: Effect;
     fetch: Effect;
   };
   reducers: {
@@ -33,44 +30,15 @@ const HeroModel: HeroModelType = {
   },
 
   effects: {
-    *query({ payload }, { call, put, select }) {
-      const data = yield call(query, payload);
-      console.log(data)
-      yield put({
-        type: 'save',
-        payload: { name: data.text },
-      });
-    },
     *fetch({ type, payload }, { put, call, select }) {
-      // const data = yield request('/api/herolist.json');
-      const data = yield request('/api/herodetails.json', {
-        method: 'POST',
-        body: JSON.stringify({
-          ename: 110,
-        }),
-      });
-      const localData = [
-        {
-          ename: 105,
-          cname: '廉颇',
-          title: '正义爆轰',
-          new_type: 0,
-          hero_type: 3,
-          skin_name: '正义爆轰|地狱岩魂',
-        },
-        {
-          ename: 106,
-          cname: '小乔',
-          title: '恋之微风',
-          new_type: 0,
-          hero_type: 2,
-          skin_name: '恋之微风|万圣前夜|天鹅之梦|纯白花嫁|缤纷独角兽',
-        },
-      ];
+      const herolist = yield call(queryHeroList);
+      const herodetails = yield call(getHeroDetails, { ename: 110 });
+      console.log(herodetails);
+
       yield put({
         type: 'save',
         payload: {
-          heros: data || localData,
+          heros: herolist,
         },
       });
     },
